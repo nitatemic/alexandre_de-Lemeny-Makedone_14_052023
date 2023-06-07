3e5b22f6036d8bc68aace8970cfe1763a8f45b23
nitatemic <alexandre@nitatemic.dev>
2023-06-07 13:24:12 +0200

    Init vite project

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+// noinspection JSCheckFunctionSignatures
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import mkcert from 'vite-plugin-mkcert';
+import autoprefixer from 'autoprefixer';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), mkcert()],
+    css: {
+        postcss: {
+            plugins: [autoprefixer],
+        },
+    },
+    server: {
+        port: 8080,
+        hot: true,
+        https: true,
+    },
+});
