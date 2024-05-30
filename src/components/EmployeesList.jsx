2e66f3d683f033c2b572d51b5635462939a77fa3
nitatemic <alexandre@nitatemic.dev>
2024-05-30 13:28:58 +0200

    Pagination

--- src/components/EmployeesList.jsx
+++ src/components/EmployeesList.jsx
@@ -10,15 +10,13 @@ export default function EmployeesList() {
   const [pageSize, setPageSize] = useState(10); // Nombre d'éléments par page
   const [currentPage, setCurrentPage] = useState(0); // Page actuelle
 
-
-
   /* Filtrer les données en fonction du terme de recherche et de la pagination */
   let filteredData = employees
-    .filter((item) => item.firstName.toLowerCase().includes(searchTerm.toLowerCase()))
+    .filter((item) => item.firstName.toLowerCase().includes(searchTerm.toLowerCase()));
 
   const totalItems = filteredData.length;
 
-/* Pagination */
+  /* Pagination */
   filteredData = filteredData.slice(currentPage * pageSize, (currentPage + 1) * pageSize);
 
   /* Gérer le changement de terme de recherche */
@@ -86,36 +84,35 @@ export default function EmployeesList() {
     return (
       <table {...getTableProps()}>
         <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                {column.render('Header')}
-                <span>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+                  <span>
                     {column.isSorted ? (column.isSortedDesc
-                        ? <FaSortDown />
-                        : <FaSortUp />)
+                      ? <FaSortDown />
+                      : <FaSortUp />)
                       : <FaSort />}
                   </span>
-              </th>
-            ))}
-          </tr>
-        ))}
+                </th>
+              ))}
+            </tr>
+          ))}
         </thead>
         <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => <td {...cell.getCellProps()}>{cell.render('Cell')}</td>)}
-            </tr>
-          );
-        })}
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => <td {...cell.getCellProps()}>{cell.render('Cell')}</td>)}
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     );
   }
-
 
   const totalPages = Math.ceil(totalItems / pageSize);
 
@@ -133,12 +130,18 @@ export default function EmployeesList() {
 
       {/* Pagination */}
       <div>
-        <button onClick={() => setCurrentPage(currentPage - 1)}
-                disabled={currentPage === 0}>Previous
+        <button
+          onClick={() => setCurrentPage(currentPage - 1)}
+          disabled={currentPage === 0}
+        >
+          Previous
         </button>
-        <span>Showing elements {currentPage * pageSize + 1} - {Math.min((currentPage + 1) * pageSize, totalItems)} of {totalItems}</span>
-        <button onClick={() => setCurrentPage(currentPage + 1)}
-                disabled={currentPage === totalPages - 1}>Next
+        <span> Showing elements {currentPage * pageSize + 1} - {Math.min((currentPage + 1) * pageSize, totalItems)} of {totalItems} </span>
+        <button
+          onClick={() => setCurrentPage(currentPage + 1)}
+          disabled={currentPage === totalPages - 1 || totalPages === 0}
+        >
+          Next
         </button>
       </div>
 
