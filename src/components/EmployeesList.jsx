5f7fccb890a0637f854e0f40a4ab4b2028c40c2a
nitatemic <alexandre@nitatemic.dev>
2024-05-30 12:04:48 +0200

    Add React-table, add sorting

--- src/components/EmployeesList.jsx
+++ src/components/EmployeesList.jsx
@@ -1,31 +1,119 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useTable, useSortBy } from 'react-table';
+import { FaSort, FaSortUp, FaSortDown } from 'react-icons/fa';
 
 export default function EmployeesList() {
+  /* Nombre d'éléments à afficher par page */
   const itemsPerPage = 10;
   const employees = useSelector((state) => state.employees);
 
   const [currentPage, setCurrentPage] = useState(1);
   const [searchTerm, setSearchTerm] = useState('');
 
-  // Filtrer les données en fonction du terme de recherche
+  /* Filtrer les données en fonction du terme de recherche */
   const filteredData = employees.filter((item) => item.firstName.toLowerCase().includes(searchTerm.toLowerCase()));
-  // Obtenir les éléments à afficher sur la page actuelle
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
   const currentData = filteredData.slice(startIndex, endIndex);
 
-  // Gérer le changement de page
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-  };
-
-  // Gérer le changement de terme de recherche
+  /* Gérer le changement de terme de recherche */
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
-    setCurrentPage(1); // Réinitialiser la page lors de la recherche
+    /* Réinitialiser la page lors de la recherche */
+    setCurrentPage(1);
   };
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'First Name',
+        accessor: 'firstName',
+      },
+      {
+        Header: 'Last Name',
+        accessor: 'lastName',
+      },
+      {
+        Header: 'Start Date',
+        accessor: 'startDate',
+      },
+      {
+        Header: 'Department',
+        accessor: 'department',
+      },
+      {
+        Header: 'Date of Birth',
+        accessor: 'dateOfBirth',
+      },
+      {
+        Header: 'Street',
+        accessor: 'street',
+      },
+      {
+        Header: 'City',
+        accessor: 'city',
+      },
+      {
+        Header: 'State',
+        accessor: 'state',
+      },
+      {
+        Header: 'Zip Code',
+        accessor: 'zipCode',
+      },
+    ],
+    [],
+  );
+
+  function Table({ columns, data }) {
+    const {
+      getTableProps,
+      getTableBodyProps,
+      headerGroups,
+      rows,
+      prepareRow,
+    } = useTable(
+      {
+        columns,
+        data,
+      },
+      useSortBy,
+    );
+
+    return (
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+                  <span>
+                    {column.isSorted ? (column.isSortedDesc
+                      ? <FaSortDown />
+                      : <FaSortUp />)
+                      : <FaSort />}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => <td {...cell.getCellProps()}>{cell.render('Cell')}</td>)}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  }
 
   return (
     <div>
@@ -36,53 +124,9 @@ export default function EmployeesList() {
         value={searchTerm}
         onChange={handleSearchChange}
       />
-      <table className="table">
-        <thead>
-        <tr>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Start Date</th>
-          <th>Department</th>
-          <th>Date of Birth</th>
-          <th>Street</th>
-          <th>City</th>
-          <th>State</th>
-          <th>Zip Code</th>
-        </tr>
-        </thead>
-        <tbody>
-        {currentData.map((employee) => (
-          <tr key={employee.id}>
-            <td>{employee.firstName}</td>
-            <td>{employee.lastName}</td>
-            <td>{employee.startDate}</td>
-            <td>{employee.department}</td>
-            <td>{employee.dateOfBirth}</td>
-            <td>{employee.street}</td>
-            <td>{employee.city}</td>
-            <td>{employee.state}</td>
-            <td>{employee.zipCode}</td>
-          </tr>
-        ))}
-        </tbody>
-      </table>
-      <ul className="pagination">
-        {Array(Math.ceil(filteredData.length / itemsPerPage))
-          .fill()
-          .map((_, index) => (
-            <li
-              key={index}
-              className={`page-item ${currentPage === index + 1 ? 'active' : ''}`}
-            >
-              <button
-                className="page-link"
-                onClick={() => handlePageChange(index + 1)}
-              >
-                {index + 1}
-              </button>
-            </li>
-          ))}
-      </ul>
+
+      <Table columns={columns} data={currentData} />
+
       <Link to="/" className="error-link">Home</Link>
     </div>
   );
